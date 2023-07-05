5fef88db992a073cae67bdc8570e29f66ab494d8
ChihYing_EC2 <spacetime0311@gmail.com>
2023-07-05 05:57:06 +0000

    feat: Create prefect project

--- batch_deployment/score.py
+++ batch_deployment/score.py
@@ -2,13 +2,23 @@
 # coding: utf-8
 
 import argparse
-import logging
 import os
 import uuid
-from typing import Dict, List
+from datetime import datetime
+from typing import Dict, List, Tuple
 
 import mlflow
 import pandas as pd
+
+# from dateutil.relativedelta import relativedelta
+from prefect import flow, get_run_logger, task
+from prefect.artifacts import create_markdown_artifact
+from prefect.context import get_run_context
+
+
+def generate_uuids(length: int) -> List[str]:
+    """Generate uuid for each record."""
+    return [str(uuid.uuid4()) for _ in range(length)]
 
 
 def read_dataframe(filename: str) -> pd.DataFrame:
@@ -37,23 +47,17 @@ def prepare_dictionaries(df: pd.DataFrame) -> List[Dict]:
     return dicts
 
 
-def generate_uuids(n):
-    return [str(uuid.uuid4()) for _ in range(n)]
-
-
 def load_model(run_id):
+    """Load model from S3 bucket."""
     logged_model = f"s3://taxi-mlops/1/{run_id}/artifacts/model"
     model = mlflow.pyfunc.load_model(logged_model)
     return model
 
 
-def apply_model(input_file, run_id, output_file):
-    df = read_dataframe(input_file)
-    dicts = prepare_dictionaries(df)
-
-    model = load_model(run_id)
-    y_pred = model.predict(dicts)
-
+def save_results(
+    df: pd.DataFrame, y_pred: List[float], run_id: str, output_file: str
+) -> pd.DataFrame:
+    """Create result dataframe and save it to parquet."""
     df_result = pd.DataFrame()
     df_result["ride_id"] = df["ride_id"]
     df_result["lpep_pickup_datetime"] = df["lpep_pickup_datetime"]
@@ -63,46 +67,100 @@ def apply_model(input_file, run_id, output_file):
     df_result["predicted_duration"] = y_pred
     df_result["diff"] = df_result["actual_duration"] - df_result["predicted_duration"]
     df_result["model_version"] = run_id
-    df_result.to_parquet(output_file, index=False)
 
+    df_result.to_parquet(output_file, index=False)
     return df_result
 
 
-def run(year, month, taxi_type, run_id):
-    # Configure logging
-    logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-    )
+@task
+def apply_model(input_file, run_id, output_file):
+    logger = get_run_logger()
 
-    # Log an informational message
-    logging.info("Starting the run...")
+    logger.info(f"Reading the data from {input_file}...")
+    df = read_dataframe(input_file)
+    dicts = prepare_dictionaries(df)
 
-    # Setup input and output file
+    logger.info(f"Loading the model with RUN_ID={run_id}...")
+    model = load_model(run_id)
+
+    logger.info("applying the model...")
+    y_pred = model.predict(dicts)
+
+    logger.info(f"Saving the result to {output_file}...")
+
+    result = save_results(df, y_pred, run_id, output_file)
+
+    start_index = (
+        input_file.rfind("_") + 1
+    )  # Find the index of the underscore before the desired portion
+    end_index = input_file.rfind(
+        "."
+    )  # Find the index of the dot before the file extension
+    year_month = input_file[
+        start_index:end_index
+    ]  # Extract the substring between the underscore and the dot
+
+    markdown_report = f"""# Prediction Report
+        ## Summary
+
+        Duration Prediction
+
+        ## Random Forest Model
+        |   Year/Month    | Mean difference |
+        |:----------------|----------------:|
+        |    {year_month}      |          {result["diff"].mean():.2f} |
+        """
+
+    create_markdown_artifact(key="duration-report", markdown=markdown_report)
+
+
+def get_paths(run_date: datetime, taxi_type: str, run_id: str) -> Tuple[str]:
+    """Getting the path for input and ouput file"""
+    # prev_month = run_date - relativedelta(months=1)
+    year = run_date.year
+    month = run_date.month
+
     input_file = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet"
-    output_file = f"output/{taxi_type}/{year:04d}-{month:02d}.parquet"
+    output_file = f"s3://taxi-mlops/output/taxi_type={taxi_type}/year={year:04d}/month={month:02d}/{run_id}.parquet"
 
-    # Setup AWS credential
+    return input_file, output_file
+
+
+@flow(name="inference")
+def ride_duration_prediction(taxi_type: str, run_id: str, run_date: datetime = None):
+    if run_date is None:
+        ctx = get_run_context()
+        run_date = ctx.flow_run.expected_start_time
+
+    input_file, output_file = get_paths(run_date, taxi_type, run_id)
     os.environ["AWS_PROFILE"] = "Profile1"
 
-    # Log another message with variables included
-    logging.info(f"Processing data for {taxi_type} taxis in {year}/{month:02d}")
-
-    result = apply_model(input_file, run_id, output_file)
-
-    # Log a final message
-    logging.info("Run completed successfully.")
-    logging.info(f"The mean difference is {result['diff'].mean():.2f} mins.")
+    apply_model(input_file=input_file, run_id=run_id, output_file=output_file)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--year", type=int, help="The year of the input data")
-    parser.add_argument("--month", type=int, help="The month of the input data")
+    parser.add_argument(
+        "--year", type=int, help="The year of the input data", default=None
+    )
+    parser.add_argument(
+        "--month", type=int, help="The month of the input data", default=None
+    )
     parser.add_argument("--taxi_type", type=str, help="The taxi type", default="green")
     # "95c848791a7642ff8c26794d43e410a8"
     parser.add_argument(
         "--run_id", type=str, help="MLflow run id for model in S3 bucket"
     )
+
     args = parser.parse_args()
-    # run(**vars(args))
-    result = run(args.year, args.month, args.taxi_type, args.run_id)
+
+    if args.month is None:
+        run_date = None
+    else:
+        run_date = datetime(year=args.year, month=args.month, day=1)
+
+    ride_duration_prediction(
+        taxi_type=args.taxi_type,
+        run_id=args.run_id,
+        run_date=run_date,
+    )
